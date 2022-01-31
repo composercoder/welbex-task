f18e6f80d3f5241a96c6b0c63439f50b51a9af90
composercoder <valentinachernova1402@gmail.com>
2022-02-01 00:58:42 +0200

    fix bug caused by wrongly passed parameters to function

--- server/modules/setPageTurnAllowance.js
+++ server/modules/setPageTurnAllowance.js
@@ -2,13 +2,13 @@ function setPageTurnAllowance(query, direction) {
   switch(direction) {
     case 'left':
       if (query.offset && query.limit) {
-        (Number(request.query.offset) - Number(request.query.limit)) > 0;
+        (Number(query.offset) - Number(query.limit)) > 0;
       } else {
         return false;
       }
     case 'right':
       if (query.offset === Number && query.limit) {
-        (Number(request.query.offset) + Number(request.query.limit)) < data.rows.length
+        (Number(query.offset) + Number(query.limit)) < data.rows.length
       } else {
         return false;
       }
