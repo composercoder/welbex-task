8729930fb197506d94b698f3caec62ee546e7e6a
composercoder <valentinachernova1402@gmail.com>
2022-02-23 11:48:44 +0200

    export createFilterParamsString function

--- client/src/controller/createFilterParamsString.js
+++ client/src/controller/createFilterParamsString.js
@@ -1,7 +1,11 @@
-function createFilterParamsString(onlyChangeOffset, dispatch, prevParams, tablePageOffset, tablePageSize, filterParameter, filterCondition, filterValue) {
+export default function createFilterParamsString(params) {
+
+  const { dispatch, filterParameter, filterCondition, filterValue, prevFilterParams, onlyChangeOffset, tablePageOffset, tablePageSize } = params;
+
   if (onlyChangeOffset) {
-    dispatch({ action: 'setFilterParams', payload: prevParams.replace(/offset=[1-9]{2}/g, 'offset=' + tablePageOffset) });
+    dispatch({ action: 'setFilterParams', payload: prevFilterParams.replace(/offset=[1-9]{2}/g, 'offset=' + tablePageOffset) });
   } else {
     dispatch({ action: 'setFilterParams', payload: `?parameter=${filterParameter.selectedOption}&condition=${filterCondition.selectedOption}&value=${filterValue}&offset=${tablePageOffset}&limit=${tablePageSize + 1}`});
   }
+
 }
