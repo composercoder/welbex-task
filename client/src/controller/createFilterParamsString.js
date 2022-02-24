632bab5fa0c6c929d4169939d14d9e6f28c893b5
composercoder <valentinachernova1402@gmail.com>
2022-02-25 01:54:38 +0200

    correct a typo, instead of passing a string as an action.type to dispatch, I was passing it as an action

--- client/src/controller/createFilterParamsString.js
+++ client/src/controller/createFilterParamsString.js
@@ -3,9 +3,9 @@ export default function createFilterParamsString(params) {
   const { dispatch, filterParameter, filterCondition, filterValue, prevFilterParams, onlyChangeOffset, tablePageOffset, tablePageSize } = params;
 
   if (onlyChangeOffset) {
-    dispatch({ action: 'setFilterParams', payload: prevFilterParams.replace(/offset=[1-9]{2}/g, 'offset=' + tablePageOffset) });
+    dispatch({ type: 'setFilterParams', payload: prevFilterParams.replace(/offset=[1-9]{2}/g, 'offset=' + tablePageOffset) });
   } else {
-    dispatch({ action: 'setFilterParams', payload: `?parameter=${filterParameter.selectedOption}&condition=${filterCondition.selectedOption}&value=${filterValue}&offset=${tablePageOffset}&limit=${tablePageSize + 1}`});
+    dispatch({ type: 'setFilterParams', payload: `?parameter=${filterParameter.selectedOption}&condition=${filterCondition.selectedOption}&value=${filterValue}&offset=${tablePageOffset}&limit=${tablePageSize + 1}`});
   }
 
 }
