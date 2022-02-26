c62058a7cf16ed1f825e3b02bfc388fdbc289616
composercoder <valentinachernova1402@gmail.com>
2022-02-27 01:28:14 +0200

    remove Table.test.js file

--- client/src/components/Table/Table.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Table from './Table';
-
-it('renders app component correctly', () => {
-  render(<Table />);
-
-  expect(screen.getByText(/Забеги Украины/i)).toBeInTheDocument();
-});
