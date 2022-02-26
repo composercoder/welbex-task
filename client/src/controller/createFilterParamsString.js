355953b4b620c010d979e64fac4d707447fa3b02
composercoder <valentinachernova1402@gmail.com>
2022-02-26 18:48:02 +0200

    correct regular expression and parameter acception

--- client/src/controller/createFilterParamsString.js
+++ client/src/controller/createFilterParamsString.js
@@ -1,10 +1,9 @@
-export default function createFilterParamsString(params) {
-
-  const { dispatch, filterParameter, filterCondition, filterValue, prevFilterParams, onlyChangeOffset, tablePageOffset, tablePageSize } = params;
+export default function createFilterParamsString({onlyChangeOffset, tablePageOffset, tablePageSize, prevFilterParams, newFilterParams, dispatch}) {
 
   if (onlyChangeOffset) {
-    dispatch({ type: 'setFilterParams', payload: prevFilterParams.replace(/offset=[1-9]{2}/g, 'offset=' + tablePageOffset) });
+    dispatch({ type: 'setFilterParams', payload: prevFilterParams.replace(/offset=(\d){1,2}/, 'offset=' + tablePageOffset) });
   } else {
+    const { filterParameter, filterCondition, filterValue } = newFilterParams;
     dispatch({ type: 'setFilterParams', payload: `?parameter=${filterParameter.selectedOption}&condition=${filterCondition.selectedOption}&value=${filterValue}&offset=${tablePageOffset}&limit=${tablePageSize + 1}`});
   }
 
